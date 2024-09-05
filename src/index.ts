300d1fa172500b41b8513f9dd41806f51568d59f
Tamago <zekun.jin@foxmail.com>
2024-09-05 10:28:26 +0800

    feat: hmr add pages

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
 import type { Compiler } from '@rspack/core'
+import { hash } from 'ohash'
 import { RspackVirtualModulePlugin } from 'rspack-plugin-virtual-module'
 import { resolveOptions } from 'unplugin-vue-router/options'
 import { type Options, createRoutesContext } from 'unplugin-vue-router'
 
-export default class RspackPluginVueRouter extends RspackVirtualModulePlugin {
+export default class RspackVueRouterPlugin extends RspackVirtualModulePlugin {
+  public name = 'RspackVueRouterPlugin'
+
   private _ctx
+  private _hash = ''
 
   constructor(options: Options = {}) {
-    super({ 'vue-router/auto-routes': '' })
+    super({ 'vue-router/auto-routes': 'export const routes = []' })
     this._ctx = createRoutesContext(resolveOptions(options))
   }
 
   async apply(compiler: Compiler) {
     super.apply(compiler)
     await this._ctx.scanPages(false)
-    this.writeModule('vue-router/auto-routes', this._ctx.generateRoutes())
+    const content = this._ctx.generateRoutes()
+    this.writeModule('vue-router/auto-routes', content)
+
+    compiler.hooks.thisCompilation.tap(this.name, async () => {
+      await this._ctx.scanPages(false)
+      const content = this._ctx.generateRoutes()
+      const newHash = hash(content)
+      if (this._hash === newHash)
+        return
+      this._hash = newHash
+      this.writeModule('vue-router/auto-routes', content)
+    })
   }
 }
